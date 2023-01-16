64739c84190c1c8f91761a08987e8bd9b56d307b
Ducpham <ducpv@beetsoft.com>
2023-01-16 10:36:09 +0700

    up  base slick slider

---
+++ common/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".btn-blue").click(function() {
+    $(this).parents().find(".modal__popup").addClass('block');
+    $(this).parents().find(".modal__content").addClass('show');
+    $(this).parents().find(".modal__close").removeClass('show');
+  });
+  $(".modal_close").click(function() {
+    $(this).parents().find(".modal__popup").removeClass('block');
+    $(this).parent(".modal__content").removeClass('show'); 
+  });
+  $(".modal__content").click(function(e) {
+    e.stopPropagation(e);
+  });
+  $(".modal__popup").click(function() {
+    $(this).removeClass('block');
+    $(this).parents().find(".modal__content").removeClass('show');      
+  });
+});
